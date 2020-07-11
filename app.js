658b3d9d8417e4a0617ff394a8e6541d93dbabe2
teezzan <teehazzan@gmail.com>
2020-07-11 09:38:08 +0100

    added cb fnction

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');})
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
+  });
 });
 
 http.listen(3000, () => {
